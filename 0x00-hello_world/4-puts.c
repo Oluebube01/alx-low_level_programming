4d33608cd0ff1bcbafa15cf02c3eaf98d563d7ac
Oluebube01 <oluebube.madu@yahoo.com>
2022-12-08 11:05:07 -0800

    putsfunction

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return: by default returns zero if no errors otherwise returns non-zero value.
+ */
+int main ()
+{
+	puts("\"Programming is like building a multilingual puzzle/n");
+	return 0;
+}
