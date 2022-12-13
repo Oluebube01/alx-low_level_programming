aebb1d307bc334a9d0533dde3560edb979af7bea
Oluebube01 <oluebube.madu@yahoo.com>
2022-12-13 08:52:57 -0800

    printalphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet-prints all alpha in lower case
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+	_putchar('\n')
