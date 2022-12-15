a3fcf2049805dca82f43470bb7b062144ec96e0f
Oluebube01 <oluebube.madu@yahoo.com>
2022-12-15 13:47:55 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,12 +4,20 @@
  * print_more_numbers - print numbers from,0 to 14, ten times.
  * Return: Always 0.
  **/
-void print_more_numbers(void);
+void print_more_numbers(void)
 {
 	int i, j;
 
-	for(i = '0'; i <= '14'; i++)
+	for (i = 0; i <= 14; i++)
 	{
-		for (j = '0'; j <= '14'; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (j >= '10
+			if (j >= 10)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
