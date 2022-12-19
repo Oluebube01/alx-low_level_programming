19e03727b4cbfa2cc2c09e27cc7888ee93d50d31
Oluebube01 <oluebube.madu@yahoo.com>
2022-12-19 15:30:53 -0800

    reset

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - wrtied the character c to stdout
+ * @c: the character to print
+ *
+ * Return: on success 1. 
+ * on error, -1 is returned, an errno is set appropriately.
+ **/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
